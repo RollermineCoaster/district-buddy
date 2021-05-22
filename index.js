91cad1fff528f4fb8d26249307d29a100bdaee61
RollermineCoaster <50976062+RollermineCoaster@users.noreply.github.com>
2021-05-22 14:25:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ var ser = restify.createServer();
 ser.use(restify.plugins.bodyParser({mapParams: true}));
 
 ser.post('/reg', function (req, res, next) {
-  pool.query('INSERT INTO public.users(name, phone, pwd)	VALUES ($1, $2, $3);', [req.params.name, req.params.phone, req.params.pwd], (err, qres) => {
-    if (err) {
-      console.log(err);
-      res.send(500);
-    } else {
-      res.send(201);
-    }
-  });
+
+  if(req.params.name&&req.params.phone&&req.params.pwd) {
+    pool.query('INSERT INTO public.users(name, phone, pwd)	VALUES ($1, $2, $3);', [req.params.name, req.params.phone, req.params.pwd], (err, qres) => {
+      if (err) {
+        console.log(err);
+        res.send(500);
+      } else {
+        res.send(201);
+      }
+    });
+  }
 
   next();
 });
+
+
 
 ser.listen(process.env.PORT || 8080, function() {
   console.log('%s listening at %s', ser.name, ser.url);
