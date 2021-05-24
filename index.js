bd93d888a5c67ec02e1bd3694932506cae0a123d
RollermineCoaster <50976062+RollermineCoaster@users.noreply.github.com>
2021-05-24 20:32:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var ser = restify.createServer();
 
 ser.pre(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS")
   next();
 })
 
